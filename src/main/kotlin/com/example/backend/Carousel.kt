f5f6ef19a20ad44eec1611008f01d10ce77a64c3
himyjan <51815522+himyjan@users.noreply.github.com>
2024-01-05 04:58:29 +0800

    upload code 1.enable cros 2. run fomatter

--- src/main/kotlin/com/example/backend/Carousel.kt
+++ src/main/kotlin/com/example/backend/Carousel.kt
@@ -1,12 +1,5 @@
 package com.example.backend
 
-data class Carousel (
-  val data: ArrayList<CarouselData>
-)
+data class Carousel(val data: ArrayList<CarouselData>)
 
-data class CarouselData (
-  val id: Long,
-  val product_id: Long,
-  val picture: String,
-  val story: String
-)
+data class CarouselData(val id: Long, val product_id: Long, val picture: String, val story: String)
